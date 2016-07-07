92fdd57e06737b33a5c11bb19ab5701837052dd5
Adrian Ojeda <achi2211@gmail.com>
2016-07-07 17:12:24 -0400

    testing php code

---
+++ index.php
@@ -0,0 +1,15 @@
+
+ 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+    <head> 
+ 
+        <meta http-equiv="Content-Type" content="text/html;charset=utf8" /> 
+        <title>Testing Capistrano</title> 
+        
+        <base href="/" /> 
+ 
+    </head> 
+    <body> 
+          <h3><?php echo "This is a Capistrano Test"; ?></h3>
+    </body> 
+</html>
